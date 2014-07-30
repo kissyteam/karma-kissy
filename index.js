952b08484f8af6569748d183fcd6d99171b28e55
元彦 <yuanyan.cao@alibaba-inc.com>
2014-07-30 23:27:04 +0800

    Served files under build.

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var path = require('path'),
     fs = require('fs');
 
-var createPattern = function(path) {
-  return {pattern: path, included: true, served: true, watched: false};
+var createPattern = function(path, included) {
+    return {pattern: path, included: included, served: true, watched: false};
 };
 
 var createFramework = function(files) {
     var kissyPath = path.dirname(require.resolve('kissy'));
-    files.unshift(createPattern(path.join(kissyPath, 'seed.js')));
+    files.unshift(createPattern(path.join(kissyPath, '**/*.js'), false));
+    files.unshift(createPattern(path.join(kissyPath,'seed.js'), true));
 };
 
 createFramework.$inject = ['config.files'];
